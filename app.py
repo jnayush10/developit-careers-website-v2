0192ceeb22b6582dac96b42e1f0aa0267b14f00c
Ayush Jain <161840923+jnayush10@users.noreply.github.com>
2024-09-07 16:46:44 +0000

    Added API Route

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)
 
@@ -26,8 +26,13 @@ JOBS = [{
 
 @app.route('/')
 def hello_developit():
-  return render_template('home.html', jobs=JOBS, company_name='Developit')
+    return render_template('home.html', jobs=JOBS, company_name='Developit')
+
+
+@app.route('/api/jobs')
+def list_jobs():
+    return jsonify(JOBS)
 
 
 if __name__ == '__main__':
-  app.run(host='0.0.0.0', port=8080)
+    app.run(host='0.0.0.0', port=8080)
